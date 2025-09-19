b68a7123ba54891dfacd3679a1af5c1b6455528c
Dev Prashant <dev.prashant1901@gmail.com>
2025-09-19 23:24:07 +0530

    bug fixes

--- src/components/AdminNav.jsx
+++ src/components/AdminNav.jsx
@@ -7,7 +7,7 @@ const AdminNav = () => {
           <span className="font-bold text-2xl text-green-600">CarbonSetu</span>
           <div className="flex-1 flex justify-end">
             <div className="flex space-x-12">
-              <a href="#" className="text-green-500 hover:text-green-600 px-6 py-2 rounded-xl text-xl font-medium">Dashboard</a>
+              <a href="/" className="text-green-500 hover:text-green-600 px-6 py-2 rounded-xl text-xl font-medium">Dashboard</a>
               <a href="#" className="text-green-500 hover:text-green-600 px-6 py-2 rounded-xl text-xl font-medium">Accounts</a>
               <a href="#" className="text-green-500 hover:text-green-600 px-6 py-2 rounded-xl text-xl font-medium">Projects</a>
               <a href="#" className="text-green-500 hover:text-green-600 px-6 py-2 rounded-xl text-xl font-medium">Credit Issuance</a>
