9bc43869b97377ec56131b54361181acf51e18f3
Dev Prashant <dev.prashant1901@gmail.com>
2025-09-26 23:48:28 +0530

    bugs

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -7,6 +7,9 @@ const HomeNav = () => {
     const handleSignup = () => {
         navigate('/signup');
     }
+    const viewRegistry = () => {
+        navigate('/registry');
+    }
   return (
     <nav className="bg-gray-100 shadow-md w-full top-0 left-0">
       <div className="w-full px-16">
@@ -14,7 +17,7 @@ const HomeNav = () => {
           <span className="font-bold text-2xl text-green-600">CarbonSetu</span>
           <div className="flex-1 flex justify-end">
             <div className="flex space-x-5">
-              <a href="/registry" className="text-green-500 font-bold hover:text-green-600 px-6 py-2 rounded-full text-xl">View Registry</a>
+              <button onClick={viewRegistry} className="text-green-500 font-bold hover:text-green-600 px-6 py-2 rounded-full text-xl">View Registry</button>
               <button onClick={handleLogin} className="px-8 py-3 text-base font-bold cursor-pointer bg-green-600 text-white rounded-full hover:bg-green-700 transition">
                 Login
             </button>
